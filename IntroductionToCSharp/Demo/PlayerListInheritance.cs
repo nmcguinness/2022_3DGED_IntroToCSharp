f814df1fb31f46e7a09a3f22c74e1605f0d1f092
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-09-23 13:29:07 +0100

    Update PlayerListInheritance.cs

--- IntroductionToCSharp/Demo/PlayerListInheritance.cs
+++ IntroductionToCSharp/Demo/PlayerListInheritance.cs
@@ -15,26 +15,14 @@
         /// <param name="p"></param>
         /// <param name="addDuplicate"></param>
         /// <returns></returns>
-        public bool Add(Player p, bool addDuplicate)
-        {
-            //jump out on one condition
-            if (Contains(p) && addDuplicate == false)
-                return false;
-
-            //in all other circumstances, add
-            Add(p);
-
-            //return true for success
-            return true;
-        }
 
         public override string ToString()
         {
             int index = 0;
-            string outStr = "Name\tHealth\tType");
+            string outStr = "Name\tHealth\tType";
             foreach (Player p in this)
             {
-                outStr += $"[{index}]: {p}");
+                outStr += $"[{index}]: {p}";
                 index++;
             }
 
